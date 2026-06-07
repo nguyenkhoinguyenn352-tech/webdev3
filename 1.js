a4270ce9ba2609bcc409aced90e5d1caf4d1e096
nguyenkhoinguyenn352-tech <nguyenkhoinguyenn352@gmail.com>
2026-06-07 20:53:30 +0700

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const target1 = document.getElementById('target');
+
+target1.innerHTML = `
+  <li>First item</li>
+  <li>Second item</li>
+  <li>Third item</li>
+`;
+
+target1.classList.add('my-list');
